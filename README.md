09af51cd488172079042d7aff4b8411e7c58fbfa
patxi <patxi@batelcon.com>
2021-10-03 09:33:00 +0200

    do-submodule.sh continues even when make fails. Added license information.

--- README.md
+++ README.md
@@ -6,3 +6,15 @@ An intervalometer application to install in the hecked camera.
 
 At this moment only a hello world skeleton wich cross-compiles to a staic binary
 using buildroot.
+
+## License
+
+### The qtinterval app in "src" folder and files in the upper level
+
+All files except those contained in the "buildroot-2021.02.5" folder are licensed
+under the MIT license. See the "COPYING" file.
+
+### Buildroot in "buildroot-2021.02.5" folder
+
+All files contained in the "buildroot-2021.02.5" folder are licensed under the
+GPLv2 license. See the "COPYING" file in that folder.
--- do-submodule.sh
+++ do-submodule.sh
@@ -8,8 +8,9 @@ if [ -d "${THIS_DIR}/${1}" ]; then
         cp -f ../0001-libxkbcommon-static-and-shared.patch package/libxkbcommon/
         cp -f ../0001-xdo-build-and-install-static-library.patch package/xdotool/
         make oldconfig
-        make
+        make || echo "Make failed but it does not matter."
         # I can't guess a better method to get the libxdo library installed in the build environment.
+        echo "Coying xdo library and header to the right place."
         cp -f output/build/xdotool-3.20160805.1/libxdo.a output/host/arm-buildroot-linux-musleabihf/sysroot/usr/lib/
         cp -f output/build/xdotool-3.20160805.1/xdo.h output/host/arm-buildroot-linux-musleabihf/sysroot/usr/include/
         ;;
